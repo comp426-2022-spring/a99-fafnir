86551a65b09dbed49c8a1dda36175464612c94ae
Savannah Ingle <sningle@email.unc.edu>
2022-04-29 20:35:45 -0400

    adding summaries for mealtimes

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const { auth } = require('express-openid-connect'); //test
 // Require additional files
 const sleepScore = require("./sleepscore.js");
 const sleepSum = require("./summary.js");
+const sleepBefore = require("./summary.js");
+const sleepAfter = require("./summary.js");
 const user_log = require('./user_log.js')
 const db = require('./database.js').user_db;
 const authRouter = require("./auth");
@@ -125,26 +127,49 @@ app.post('/sleep/score/', function (req, res) {
 })
 
 app.post('/sleep/summary/', function (req, res) {
-    const stmt = db.prepare("SELECT wake_up_time, bedtime FROM userinfo WHERE username = '" + req.body.userName + "' AND password = '" + req.body.passWord + "' ORDER BY id DESC").all();
+    const stmt = db.prepare("SELECT meal_start_time, meal_end_time, wake_up_time, bedtime FROM userinfo WHERE username = '" + req.body.userName + "' AND password = '" + req.body.passWord + "' ORDER BY id DESC").all();
     var json = JSON.parse(JSON.stringify(stmt))
     var i = 0;
     console.log(json);
     var sum = 0;
+    var before_sum = 0;
+    var after_sum = 0;
     var length = Object.keys(json).length
+    var before_length = Object.keys(json).length
+    var after_length = Object.keys(json).length
 
     while (i < Object.keys(json).length) {
         const diff = sleepSum(stmt[i].bedtime, stmt[i].wake_up_time);
+        const meal_before_session = sleepBefore(stmt[i].bedtime, stmt[i].meal_start_time)
+        const meal_after_session = sleepAfter(stmt[i].wake_up_time, stmt[i].meal_end_time)
         if (diff != null) {
             sum += diff;
         } else {
             length--;
+        }
+        if (meal_before_session != null) {
+            before_sum += meal_before_session;
+        } else {
+            before_length--;
+        }
+        if (meal_after_session != null) {
+            after_sum += meal_after_session;
+        } else {
+            after_length--;
         }
         i++;
     }
     
     var avg = sum / length;
     var avg = avg / 100
-    res.status(200).json({"average":avg});
+
+    var before_avg = before_sum / before_length
+    var before_avg = before_avg / 100
+
+    var after_avg = after_sum / after_length
+    var after_avg = after_avg / 100
+
+    res.status(200).json({"average":avg, "before":before_avg, "after":after_avg});
 })
 
 // Endpoint that returns a 404 if endpoint does not exist
--- summary.js
+++ summary.js
@@ -13,4 +13,22 @@ function sleepSum(sleepTime, wakeTime) {
     }
 }
 
-module.exports = sleepSum
+function sleepBefore(bedtime, mealTime) {
+    if (bedtime != null && mealTime != null) {
+        const time = bedtime - mealTime
+        return time
+    } else {
+        return null;
+    }
+}
+
+function sleepAfter(wakeTime, mealTime) {
+    if (mealTime != null && wakeTime != null) {
+        const time = mealTime - wakeTime;
+        return time;
+    } else {
+        return null;
+    }
+}
+
+module.exports = sleepSum, sleepBefore, sleepAfter
